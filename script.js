c286103b2003dcb399d13153ff6fc0ce68fc6384
padamczyk <padamczyk7@gmail.com>
2021-01-11 06:43:44 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() { 
+    $('#one').fadeIn(2000);
+  
+});
+
+function setVisibility(id, visibility) {
+document.getElementById(id).style.display = visibility;
+};
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-59233606-1', 'auto');
+  ga('send', 'pageview');
